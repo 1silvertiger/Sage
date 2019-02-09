e9ee64e88919b81e8d352b00acf6b35a8ad8760a
Dakota Dalton <1silvertiger@gmail.com>
2019-02-09 17:54:52 -0600

    Changed pages to EJS

---
+++ src/public/scripts/index.ejs.js
@@ -0,0 +1,28 @@
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+
+    var data = {
+        idtoken: googleUser.getAuthResponse().id_token,
+        firstName: profile.getGivenName(),
+        lastName: profile.getFamilyName(),
+        imageUrl: profile.getImageUrl(),
+        email: profile.getEmail()
+    };
+
+    $.ajax({
+        type: "POST",
+        url: URL + '/tokensignin', 
+        data: JSON.stringify(data),
+        dataType: "text",
+        headers: {'Content-Type': 'application/json'},
+        success: function(data, status) {
+            console.log("success");
+            window.location.replace(URL + "/home");
+        },
+        error: function(jqXHR, status, error) {
+            //TODO: handle error
+            alert("An error occurred: \n" + status + "\n" + error);
+        }
+    });
+    
+}
