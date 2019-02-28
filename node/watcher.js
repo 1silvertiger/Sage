c4bce4eb4efe2eb506a9f210fa412fffca650af0
Dakota Dalton <1silvertiger@gmail.com>
2019-02-28 00:19:16 -0600

    Webhook

--- node/watcher.js
+++ node/watcher.js
@@ -1,6 +1,11 @@
 const https = require('https');
 
-https.createServer(function (req, res) {
+const options = {
+    key: fs.readFileSync('../../../../../etc/letsencrypt/live/sage-savings.com/privkey.pem'),
+    cert: fs.readFileSync('../../../../../etc/letsencrypt/live/sage-savings.com/fullchain.pem'),
+}
+
+https.createServer(options, function (req, res) {
     req.on('data', function (chunk) {
         console.log(req.body);
         exec('sh master-updated.sh');
