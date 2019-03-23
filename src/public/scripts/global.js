b68479db30d1bb09f5f9c4184bbc1885ce368143
Dakota Dalton <1silvertiger@gmail.com>
2019-03-23 00:15:16 -0500

    Logging out
    Closes #27

--- src/public/scripts/global.js
+++ src/public/scripts/global.js
@@ -1,4 +1,24 @@
 $(document).ready(function () {
+    $("#logout").click(function () {
+        $.ajax({
+            type: "POST",
+            url: URL + '/logout',
+            data: null,
+            dataType: "text",
+            headers: { 'Content-Type': 'application/json' },
+            credentials: 'same-origin',
+            xhrFields: {
+                withCredentials: true
+            },
+            success: function (data, status) {
+                // window.location.replace(URL);
+            },
+            error: function (jqXHR, status, error) {
+                //TODO: handle error
+                alert("An error occurred: \n" + status + "\n" + error);
+            }
+        });
+    });
     //Materialize component initialization
     //M.AutoInit();
     // $('.sidenav').sidenav();
