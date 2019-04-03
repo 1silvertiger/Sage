e27723361a2c3b274d8421120dfcb6639857337b
Dakota Dalton <1silvertiger@gmail.com>
2019-04-02 20:18:07 -0500

    Got user tags working
    Added remaining fields (piggy banks, bills) to user

--- src/model/userDao.js
+++ src/model/userDao.js
@@ -2,10 +2,15 @@ const Dao = require('./dao');
 const User = require('./user.js');
 const Item = require('./item');
 const Budget = require('./budget');
+const Tag = require('./tag');
+// const PiggyBank = require('./piggyBank');
 
 const USER_INDEX = 0;
-const ITEMS_INDEX = 1;
-const BUDGET_ITEMS_INDEX = 2;
+const TAGS_INDEX = 1
+const ITEMS_INDEX = 2;
+const BUDGET_ITEMS_INDEX = 3;
+const PIGGY_BANK_INDEX = 4;
+const PIGGY_BANK_TAGS_INDEX = 5;
 
 module.exports = class UserDao extends Dao {
     constructor(pPool) {
@@ -41,10 +46,19 @@ module.exports = class UserDao extends Dao {
                     console.log('rows: ');
                     console.log(rows);
                     if (rows[0].length > 0) {
+                        //Tags
+                        let tags = new Array();
+                        for (let i = 0; i < rows[TAGS_INDEX].length; i++) {
+                            tags.push(new Tag(rows[TAGS_INDEX][i].id, rows[TAGS_INDEX][i].userId, rows[TAGS_INDEX][i].name));
+                        }
+
+                        //Plaid items
                         let items = new Array();
                         for (let i = 0; i < rows[ITEMS_INDEX].length; i++) {
                             items.push(new Item(rows[ITEMS_INDEX][i].itemId, rows[ITEMS_INDEX][i].accessToken, null, rows[ITEMS_INDEX][i].lastSync, new Array()));
                         }
+
+                        //Budget items
                         let budgetItems = new Array();
                         for (let i = 0; i < rows[BUDGET_ITEMS_INDEX].length; i++) {
                             budgetItems.push(new Budget(
@@ -56,7 +70,7 @@ module.exports = class UserDao extends Dao {
                                 , rows[BUDGET_ITEMS_INDEX][i].numOfPeriods
                             ));
                         }
-                        resolve(new User(rows[USER_INDEX][0].googleId, rows[USER_INDEX][0].firstName, rows[USER_INDEX][0].lastName, rows[USER_INDEX][0].imageUrl, rows[USER_INDEX][0].email, items, budgetItems));
+                        resolve(new User(rows[USER_INDEX][0].googleId, rows[USER_INDEX][0].firstName, rows[USER_INDEX][0].lastName, rows[USER_INDEX][0].imageUrl, rows[USER_INDEX][0].email, tags, items, budgetItems));
                     } else
                         resolve(null);
                     conn.end();
