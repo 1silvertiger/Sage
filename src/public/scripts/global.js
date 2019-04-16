55cb73e9902ed2e0e24a4e20309569a51007a4ad
Dakota Dalton <1silvertiger@gmail.com>
2019-04-16 18:11:49 -0500

    Added service worker
    Closes #43

--- src/public/scripts/global.js
+++ src/public/scripts/global.js
@@ -1,5 +1,17 @@
 const tagMap = new Object();
 
+//Register service worker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/serviceWorker.js')
+        .then(function (reg) {
+            // registration worked
+            console.log('Registration succeeded. Scope is ' + reg.scope);
+        }).catch(function (error) {
+            // registration failed
+            console.log('Registration failed with ' + error);
+        });
+}
+
 $(document).ready(function () {
     refreshTagMap();
 
