3cfe4ea1979abff5f86b9b4a3c2fd471a284b65a
Dakota Dalton <1silvertiger@gmail.com>
2019-04-15 19:23:33 -0500

    Got tag adding in budget items
    For #45

--- src/model/budgetDao.js
+++ src/model/budgetDao.js
@@ -15,29 +15,37 @@ module.exports = class BudgetDao extends Dao {
         const tagDao = this.tagDao;
         return new Promise(function (resolve, reject) {
             const params = [
-                budget.id || null, 
-                budget.userId, 
-                budget.periodId, 
+                budget.id || null,
+                budget.userId,
+                budget.periodId,
                 budget.tag ? budget.tag.id : null,
-                budget.name, 
-                budget.amount, 
+                budget.name,
+                budget.amount,
                 budget.numOfPeriods
             ];
             const promises = [
-                pool.query(Dao.composeQuery('createOrUpdateBudgetItem', params), params)    
+                pool.query(Dao.composeQuery('createOrUpdateBudgetItem', params), params)
             ];
             if (budget.tags.length) {
                 promises.push(tagDao.createOrUpdateBatch(budget.tags));
             }
 
             Promise.all(promises).then(values => {
+                if (values[1].length) {
+                    const ids = new Array();
+                    for (let i = 0; i < values[1].length; i++)
+                        ids.push([values[0][0][0].id, values[1][i].id]);
+                    tagDao.tagBudgetItemsBatch(ids).catch(err => {
+                        Dao.handleQueryCatch(err);
+                    });
+                }
                 resolve(new Budget(
-                    values[0][0][0].id, 
-                    values[0][0][0].userId, 
-                    values[0][0][0].periodId, 
+                    values[0][0][0].id,
+                    values[0][0][0].userId,
+                    values[0][0][0].periodId,
                     new Tag(values[0][1][0].id, values[0][1][0].userId, values[0][1][0].name),
-                    values[0][0][0].name, 
-                    values[0][0][0].amount, 
+                    values[0][0][0].name,
+                    values[0][0][0].amount,
                     values[0][0][0].numOfPeriods,
                     values[1] || new Array()
                 ));
@@ -65,7 +73,7 @@ module.exports = class BudgetDao extends Dao {
 
     deleteBatch(budgetItemIds) {
         const pool = this.pool;
-        return new Promise(function(resolve, reject) {
+        return new Promise(function (resolve, reject) {
             pool.batch('CALL deleteBudgetItem(?)', budgetItemIds).then(rows => {
                 resolve(true);
             }).catch(err => {
