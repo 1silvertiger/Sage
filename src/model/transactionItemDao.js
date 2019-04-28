2d1099bd70a1d1ab567505cbca7c29a086a57b36
Dakota Dalton <1silvertiger@gmail.com>
2019-04-28 01:13:57 -0500

    Transaction items almost working
    I got one to save with no tags and Dec 31 1969 as the date, but now they don't save and the .save() function isn't being called.
    For #20

--- src/model/transactionItemDao.js
+++ src/model/transactionItemDao.js
@@ -1,10 +1,12 @@
 const Dao = require('./dao');
+const TagDao = require('./tagDao');
 const TransactionItem = require('./transactionItem');
 
 module.exports = class TransactionItemDao extends Dao {
     constructor(pPool) {
         super(pPool);
         this.Dao = new Dao(pPool);
+        this.tagDao = new TagDao(pPool);
     }
 
     createOrUpdate(transactionItem) {
@@ -15,15 +17,83 @@ module.exports = class TransactionItemDao extends Dao {
             transactionItem.amount,
             transactionItem.note
         ]
-        return new Promise(function(resolve, reject) {
+        return new Promise(function (resolve, reject) {
             pool.query(Dao.composeQuery('createOrUpdateTransactionItem', params), params).then(rows => {
-                const temp = new TransactionItem();
+                const temp = new TransactionItem(
+                    rows[i][0].id,
+                    rows[0][0].transactionId,
+                    rows[0][0].amount,
+                    rows[0][0].note,
+                    new Date(rows[0][0].appliedDate),
+                    rows[0][0].default === 1,
+                    new Array()
+                );
                 resolve(temp);
             }).catch(err => {
                 console.log(err);
                 resolve(null);
             });
         })
+    }
+
+    save(transactionId, transactionItems) {
+        const pool = this.pool;
+        const tagDao = this.tagDao;
+        return new Promise(function (resolve, reject) {
+            pool.query('CALL deleteAllTransactionItemsByTransactionId(?)', [transactionId]).then(rows => {
+                if (transactionItems.length) {
+                    const params = new Array();
+                    for (let i = 0; i < transactionItems.length; i++)
+                        params.push([
+                            transactionItems[i].id || null,
+                            transactionItems[i].transactionId,
+                            transactionItems[i].amount,
+                            transactionItems[i].appliedDate,
+                            transactionItems[i].default
+                        ]);
+                    pool.batch(Dao.composeQuery('createOrUpdateTransactionItem', params[0]), params).then(rows => {
+                        const newTransactionItems = new Array();
+                        for (let i = 0; i < rows.length; i += 2) {
+                            if (transactionItems[i / 2].tags.length) {
+                                tagDao.createOrUpdateBatch(transactionItems[i / 2].tags).then(tags => {
+                                    newTransactionItems.push(new TransactionItem(
+                                        rows[i][0].id,
+                                        rows[i][0].transactionId,
+                                        rows[i][0].amount,
+                                        rows[i][0].note,
+                                        new Date(rows[i][0].appliedDate),
+                                        rows[i][0].default === 1,
+                                        tags
+                                    ));
+                                }).catch(err => {
+                                    resolve(null);
+                                    Dao.handleQueryCatch(err);
+                                });
+                            } else {
+                                newTransactionItems.push(new TransactionItem(
+                                    rows[i][0].id,
+                                    rows[i][0].transactionId,
+                                    rows[i][0].amount,
+                                    rows[i][0].note,
+                                    new Date(rows[i][0].appliedDate),
+                                    rows[i][0].default === 1,
+                                    new Array()
+                                ));
+                            }
+                        }
+                        resolve(newTransactionItems);
+                    }).catch(err => {
+                        resolve(null);
+                        Dao.handleQueryCatch(err);
+                    });
+                } else {
+                    resolve(new Array());
+                }
+            }).catch(err => {
+                resolve(null);
+                Dao.handleQueryCatch(err);
+            });
+        });
     }
 
     createDefault(transaction) {
@@ -46,7 +116,7 @@ module.exports = class TransactionItemDao extends Dao {
 
     delete(id) {
         const pool = this.pool;
-        return new Promise(function(resolve, reject) {
+        return new Promise(function (resolve, reject) {
             pool.query('CALL deleteTransactionItem(?)', [id]).then(rows => {
                 resolve(true);
             }).catch(err => {
